1e63227b09ce1d25dfb4a0abe10c00ee829da6ec
Cédric Jeanneret <tengu@ethack.org>
2017-03-19 07:50:06 +0100

    added some progress and other things for git

--- docker-entrypoint.d/10-clone.sh
+++ docker-entrypoint.d/10-clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Cloning ${GIT_REMOTE} to /webapp"
-git clone ${GIT_REMOTE} /webapp
+git clone --progress --verbose ${GIT_REMOTE} /webapp
 cd /webapp
 echo "Setting revision to ${REVISION}"
 git reset --hard ${REVISION}
